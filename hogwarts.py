41482d751dd9294a86f105d63a3c49110ff56985
Bryan Sory <superbee45@gmail.com>
2022-09-01 18:17:07 -0500

    python dabbling

---
+++ hogwarts.py
@@ -0,0 +1,8 @@
+students = ["Hermione", "Harry", "Ron"]
+# for student in students:
+#     print (student)
+
+# for i in range(len(students)):
+#     print(i, students[i])
+# len returns the length of the number of items in an object
+
